ec2237a582a35f97d64fa3fee77c3ba1db614475
erb <erb@benae.xyz>
2024-02-07 09:56:10 -0500

    fixed nat_gateway_id to gateway_id

--- aws_network/main.tf
+++ aws_network/main.tf
@@ -59,7 +59,7 @@ resource "aws_route_table" "public_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_internet_gateway.internet_gateway.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
   tags = {
